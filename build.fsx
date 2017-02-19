40f426c4c435f9b51a60b08f94440cc73fb86537
jindraivanek <jindra.ivanek@gmail.com>
2017-02-19 12:31:10 +0100

    generate individual posts

--- build.fsx
+++ build.fsx
@@ -13,6 +13,11 @@ type Category = {
     Name: string
     Active: bool
 }
+
+type Post = {
+    Filename: string
+    Html: string
+}
 let source = __SOURCE_DIRECTORY__
 let outputPath = source @@ Config.outputPath
 
@@ -20,23 +25,29 @@ Target "Build" ( fun _ ->
     CreateDir outputPath
 
     let saveOutput path text = 
+        CreateDir ((fileInfo(outputPath @@ path).Directory.FullName))
         printfn "Creating %s." path
         File.WriteAllText(outputPath @@ path, text)
 
     let categories = DirectoryInfo(source @@ "content").GetDirectories() |> Seq.map (fun d -> d.Name) |> Seq.toList
 
+    let getContent path =
+        let f = FileInfo(path)
+        let create x =
+            { Filename = f.Name.Remove(f.Name.Length - f.Extension.Length)
+              Html = Literate.WriteHtml x }
+        match f.Extension with 
+        | ".fsx" -> Literate.ParseScriptFile(f.FullName) |> create |> Some
+        | ".md" -> Literate.ParseMarkdownFile(f.FullName) |> create |> Some
+        | _ -> printfn "Ignore %s %s" f.FullName f.Extension; None
+    
     let getPosts cat = 
         DirectoryInfo(source @@ "content" @@ cat).GetFiles()
         |> Seq.sortByDescending(fun f -> f.CreationTime)
-        |> Seq.choose (fun (f:FileInfo) -> 
-            match f.Extension with 
-            | ".fsx" -> Literate.ParseScriptFile(f.FullName) |> Some
-            | ".md" -> Literate.ParseMarkdownFile(f.FullName) |> Some
-            | _ -> printfn "Ignore %s %s" f.FullName f.Extension; None)
-        |> Seq.map Literate.WriteHtml
+        |> Seq.choose (fun (f:FileInfo) -> getContent (f.FullName))
         |> Seq.toArray
 
-    let rec fue page path =
+    let rec fue extraF page layoutPath =
         init
         |> add "site.title" Config.siteTitle
         |> add "site.tagline" Config.siteTagLine
@@ -45,19 +56,24 @@ Target "Build" ( fun _ ->
         |> add "twitterUrl" Config.twitterUrl
         |> add "page.title" page
         |> add "isHomePage" (page="index")
-        |> add "include" (fue page)
+        |> add "include" (fue extraF page)
         |> add "categories" (categories |> Seq.map (fun c -> {Name = c; Active = c=page}))
         |> add "getPosts" getPosts
         |> add "formatTime" (fun (format:string) -> System.DateTime.Now.ToString format)
         |> add "eq" (fun (x,y) -> x=y)
-        |> fromFile (source @@ "layouts" @@ path)
+        |> extraF
+        |> fromFile (source @@ "layouts" @@ layoutPath)
 
     "index" :: categories
     |> Seq.map (fun x -> x, x + ".html")
-    |> Seq.iter (fun (x, path) -> fue x path |> saveOutput path)
+    |> Seq.iter (fun (x, path) -> fue id x path |> saveOutput path)
+
+    
+
+    categories
+    |> Seq.iter (fun c -> getPosts c |> Seq.iter (fun p -> fue (add "content" p.Html) p.Filename "post.html" |> saveOutput (c + "_" + p.Filename + ".html")))
 
     CopyRecursive (source @@ "include") outputPath true |> ignore
-    
 )
 
 Target "Deploy" (fun _ ->
